978250204d9b7132c7286c1a9f3c670abe503ecc
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-04-21 07:03:23 -0300

    Add client for loading actual data

---
+++ lib/carbon_intensity/client.ex
@@ -0,0 +1,50 @@
+defmodule CarbonIntensity.Client do
+  @moduledoc """
+  Loads data from Carbon Intensity API
+
+  https://api.carbonintensity.org.uk/intensity
+  """
+
+  @type error :: :not_found | :request_error | :malformed
+
+  @api_url "https://api.carbonintensity.org.uk/intensity"
+  @number_of_retries 5
+
+  @spec actual ::
+          {:error, error()}
+          | {:ok, %{from: binary(), to: binary(), actual_intensity: pos_integer()}}
+  def actual do
+    with {:ok, %{"data" => [result]}} <- load_actual() do
+      parse_result(result)
+    end
+  end
+
+  defp parse_result(%{"from" => from, "to" => to, "intensity" => %{"actual" => actual}}) do
+    {:ok, %{from: from, to: to, actual_intensity: actual}}
+  end
+
+  defp parse_result(_other), do: {:error, :malformed}
+
+  defp load_actual(), do: load_url(@api_url)
+
+  defp load_url(url, actual_retry \\ 0)
+
+  defp load_url(url, actual_retry)
+       when actual_retry < @number_of_retries do
+    response = Mojito.request(:get, url)
+
+    case response do
+      {:ok, %{status_code: 200, body: body}} ->
+        Jason.decode(body)
+
+      {:ok, %{status_code: 404}} ->
+        {:error, :not_found}
+
+      {_, _response} ->
+        load_url(url, actual_retry + 1)
+    end
+  end
+
+  defp load_url(_url, actual_retry) when actual_retry == @number_of_retries,
+    do: {:error, :request_error}
+end
