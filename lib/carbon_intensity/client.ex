7171ca6c59b5d8e82c2ae8630fcd6b6783ff7929
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-04-21 21:20:54 -0300

    rviews specs for Client behavior

--- lib/carbon_intensity/client.ex
+++ lib/carbon_intensity/client.ex
@@ -1,6 +1,7 @@
 defmodule CarbonIntensity.Client do
-  @type error :: :not_found | :request_error | :malformed
+  @type query_error :: :not_found | :request_error | Jason.DecodeError.t()
+  @type error :: query_error | :malformed
   @type data :: %{from: binary(), to: binary(), actual_intensity: pos_integer()}
 
-  @callback actual() :: {:error, error()} | {:ok, data}
+  @callback actual() :: {:error, error()} | {:ok, data()}
 end
