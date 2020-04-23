b530b5938ee708d0131da1cd5e56d1ab1fccd1d4
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-04-23 07:53:36 -0300

    Fixex logging issues

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule CarbonIntensity.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:lager, :logger, :amqp],
       mod: {CarbonIntensity.Application, []}
     ]
   end
